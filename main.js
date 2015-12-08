8b0141734e0e3528f0acfb5c484e5907865d0d1b
takayukioda <takayuki@odashi.me>
2015-12-08 17:12:00 +0900

    Update flow to connect, and add data sending
    To manage peer connection more easier, I added "connect" button.
    Also implement connection data receive behaviour correctly.

--- main.js
+++ main.js
@@ -1,11 +1,54 @@
-(function (global, exports, undefined) {
+var Way = (function (global, document, exports, undefined) {
   'use strict';
   loadJson('./specifications/env.json', function (env) {
-    exports.env = env;
+    console.log('environment loaded');
+    var
+      apikey = env.SKYWAY_API_KEY,
+      peer = exports.peer = new Peer({key: apikey}),
+      connections = exports.connections = [],
+      display = document.getElementById('chat-display'),
+      message = document.getElementById('chat-message');
+
+    document.getElementById('peer-connect').onclick = function peerConnect (event) {
+      peer.listAllPeers(function (list) {
+        var others = list.filter(function (p, idx) {
+          return p !== peer.id;
+        });
+        console.log('List all Peers', list);
+        console.log('List other Peers', others);
+        others.forEach(function (p) {
+          exports.connections.push(peer.connect(p));
+        });
+      });
+    };
+    document.getElementById('chat-send').onclick = function sendMessage (event) {
+      console.log("Message text:", message.value);
+      connections.forEach(function (conn) {
+        console.log("Sending message to:", conn.peer);
+        conn.send(message.value);
+      });
+    };
+
+    peer.on('open', function (id) {
+      console.log('Peer opened with ID:', id);
+    }).on('close', function () {
+      console.log('Peer connection closed');
+    }).on('connection', function (conn) {
+      console.log('Peer connected:', conn);
+      conn.on('open', function () {
+        console.log('Connection opened');
+      }).on('close', function () {
+        console.log('Connection closed');
+      }).on('data', function (data) {
+        console.log('Connection data received', data);
+      }).on('error', function (err) {
+        console.log('Connection error occurred', err);
+      });
+    }).on('error', function (err) {
+      console.log('Peer error occurred:', err);
+    });
   });
-  var
-    apikey = "",
-    dummy = "mummy";
-    //peer = new Peer({key: apikey});
-  console.log(this.env);
-})(this, {})
+  return exports;
+})(this, this.document, {})
+
+console.log(Way);

f863bdeb7f8d228866e344a75a57433b94fa2aea
takayukioda <takayuki@odashi.me>
2016-01-08 13:32:47 +0900

    Add remove unnecessary lines

--- main.js
+++ main.js
@@ -11,17 +11,6 @@ app.get('/', function(req, res) {
 app.get(/.*\.(html|js|css|json)/, function(req, res) {
   res.sendFile(path.join(__dirname + req.url));
 });
-app.get('/chat.html', function(req, res) {
-  res.sendFile(path.join(__dirname + '/chat.html'));
-});
-
-app.get('/load.js', function(req, res) {
-  res.sendFile(path.join(__dirname + '/load.js'));
-});
-
-app.get('/chat.js', function(req, res) {
-  res.sendFile(path.join(__dirname + '/chat.js'));
-});
 
 app.listen(3000);
 console.log("Running server...");
